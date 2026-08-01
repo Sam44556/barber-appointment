97ead34c8022dd5805aad6c22d7ba2e0f87a5bf2
sami-g <samuelgirma888@gmail.com>
2026-08-01 17:58:25 +0300

    ci: add GitHub Actions CI pipeline 3

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findUnique: jest.fn(),
+              create: jest.fn(),
+              update: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -16,3 +29,4 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 });
+
